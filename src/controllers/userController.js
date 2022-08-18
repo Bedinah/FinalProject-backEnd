b0e9f7526803b3268bee1758898427b08e1b7203
bazubagir <bazubedinah@gmail.com>
2022-08-18 12:54:07 +0200

    rebasing code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,79 +1,76 @@
 import UserServiceS from "../services/userServices";
-import handlePassword from"../utils/handlePassword";
-import TokenAuth from "../utils/Token"
-class UserController{
+import handlePassword from "../utils/handlePassword";
+import TokenAuth from "../utils/Token";
+class UserController {
   //login controllers
-  static async loginUserController(req,res){
+  static async loginUserController(req, res) {
     const user = await UserServiceS.loginUser(req);
-    if(!user){
+    if (!user) {
       return res.status(400).json({
-        message:"user is not exist"
-      })
+        message: "user is not exist",
+      });
     }
-    if(handlePassword.checkPassword(user.password,req.body.password)){
-       const token=TokenAuth.generateToken({
-         phone:user.phone,
-        email:user.email,
-        picture:user.picture,
-        names:user.names,
-       role:user.role,
-     })
+    if (handlePassword.checkPassword(user.password, req.body.password)) {
+      const token = TokenAuth.generateToken({
+        phone: user.phone,
+        email: user.email,
+        picture: user.picture,
+        names: user.names,
+        role: user.role,
+      });
       return res.status(200).json({
-        message:"logged in successfully",token
-     })
+        message: "logged in successfully",
+        token,
+      });
+    } else {
+      return res.status(401).json({
+        message: "failed to login password is wrong",
+      });
     }
-    else{
-  return res.status(401).json({
-      message:"failed to login password is wrong",
-    
-    });
-   }
   }
-//register user
-  static async registerUser(req,res){
+  //register user
+  static async registerUser(req, res) {
     const newUser = await UserServiceS.userRegister(req);
-    if(!newUser){
+    if (!newUser) {
       return res.status(401).json({
-        message:"failed to register"
-      })
+        message: "failed to register",
+      });
     }
     return res.status(201).json({
-      message:"success",
-      data:newUser,
+      message: "success",
+      data: newUser,
     });
   }
-  static async getAll(req,res){
+  static async getAll(req, res) {
     const newUser = await UserServiceS.getAll();
-    if(!newUser){
-      return res.status(400).json({message:"failed to getAll"})
+    if (!newUser) {
+      return res.status(400).json({ message: "failed to getAll" });
     }
-    return res.status(200).json({ message:"success",data:newUser,});
+    return res.status(200).json({ message: "success", data: newUser });
   }
-  static async deleteUser(req,res){
+  static async deleteUser(req, res) {
     const newUser = await UserServiceS.deleteUser(req);
-    if(!newUser){
+    if (!newUser) {
       return res.status(401).json({
-        message:"failed to deleteUser"
-      })
+        message: "failed to deleteUser",
+      });
     }
     return res.status(201).json({
-      message:"success",
-      data:newUser,
+      message: "success",
+      data: newUser,
     });
   }
-  static async updateUser(req,res){
+  static async updateUser(req, res) {
     const newUser = await UserServiceS.updateUser(req);
-    if(!newUser){
+    if (!newUser) {
       return res.status(401).json({
-        message:"failed to updateUser"
-      })
+        message: "failed to updateUser",
+      });
     }
     return res.status(201).json({
-      message:"success",
-      data:newUser,
+      message: "success",
+      data: newUser,
     });
   }
-
-
 }
- export default UserController;
+export default UserController;
