6df8989874cf66c240662ef53c9cbe714938a49c
bazubagir <bazubedinah@gmail.com>
2022-08-22 15:34:09 +0200

    ft models and middlewares

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,8 @@ const route = Router();
 
 route.post("/user/login", UserController.loginUserController);
 
-route.post( "/user/create",
+route.post(
+  "/user/create",
 
   Validator.newAccountRules(),
   Validator.validateInput,
@@ -22,29 +23,24 @@ route.post( "/user/create",
 
 route.post("/homepage/create", homeController.createHome);
 route.post("/nutrition/create", nutritionController.createNutrition);
-route.patch("/home/:id", homeController.updateHome);
-
-route.post("/nutrition/:id", nutritionController.updateNutrition);
-
-
 route.post("/lessons/create", lessonsController.LessonsCreate);
 
-route.patch("/homepage/update", homeController.updateHome);
 route.patch("/lesson/update/:id", lessonsController.updateLessons);
-
-
+route.patch("/nutrition/:id", nutritionController.updateNutrition);
+route.patch("/home/:id", homeController.updateHome);
 
 route.get("/user/getAll", UserController.getAll);
 route.get("/nutrition/getAll", nutritionController.getAll);
 route.get("/lesson/getAll", lessonsController.getAll);
 route.get("/home/getAll", homeController.getAll);
 
-route.get("/home/:id", homeController.getOnehomepage);
-
 route.delete("/home/:id", homeController.deleteHomepage);
+route.delete("/nutrition/:id", nutritionController.deletenutrition);
 
 route.get("/user/getOne/:id", UserController.getOneUser);
 route.get("/Lessons/getOne/:id", lessonsController.getOnelessons);
+route.get("/home/:id", homeController.getOnehomepage);
+route.get("/nutrition/:id", nutritionController.getOnenutrition);
 
 route.delete(
   "/user/:id",
@@ -53,6 +49,4 @@ route.delete(
   UserController
 );
 
-
 export default route;
-
